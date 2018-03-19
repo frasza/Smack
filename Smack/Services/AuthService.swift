e20f4ebd342be75f92c6dc9fd0e7b9d188f08d1e
Zan Fras <zan.fraas@gmail.com>
2018-03-19 17:43:58 +0100

    create user registration method

---
+++ Smack/Services/AuthService.swift
@@ -0,0 +1,79 @@
+//
+//  AuthService.swift
+//  Smack
+//
+//  Created by Žan Fras on 19/03/2018.
+//  Copyright © 2018 Žan Fras. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    //MARK: - Register Methods
+    /***************************************************************/
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            
+            if response.result.error != nil {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            } else {
+                completion(true)
+            }
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+}
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://smackslovenia.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 let GO_TO_LOGIN = "goToLogin"
 let GO_TO_CREATEACCOUNT = "goToCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
