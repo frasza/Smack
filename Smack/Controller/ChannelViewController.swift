812e2a4565795b50877894e8d4a7ed3fd63a3be0
Zan Fras <zan.fraas@gmail.com>
2018-03-22 21:58:24 +0100

    set login view

--- Smack/Controller/ChannelViewController.swift
+++ Smack/Controller/ChannelViewController.swift
@@ -14,6 +14,7 @@ class ChannelViewController: UIViewController {
     /***************************************************************/
     
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var userImage: CircleImage!
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {}
     
@@ -22,10 +23,24 @@ class ChannelViewController: UIViewController {
         super.viewDidLoad()
 
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(userDataDidChange), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
     
     @IBAction func loginButtonPressed(_ sender: UIButton) {
         performSegue(withIdentifier: GO_TO_LOGIN, sender: self)
+    }
+    
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginButton.setTitle(UserDataService.instance.name, for: .normal)
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            userImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginButton.setTitle("Login", for: .normal)
+            userImage.image = UIImage(named: "menuProfileIcon")
+            userImage.backgroundColor = UIColor.clear
+        }
     }
     
 
